d8ed7a8254d69b19ea9fa030948de80f07cb44d1
xiezongyu <xiezongyu@changingedu.com>
2018-11-29 13:55:42 +0800

    test1.0.4

--- root-app/app/src/main/java/com/zylear/root/rootapp/constants/AppConstant.java
+++ root-app/app/src/main/java/com/zylear/root/rootapp/constants/AppConstant.java
@@ -2,7 +2,7 @@ package com.zylear.root.rootapp.constants;
 
 public class AppConstant {
 
-    public static final String VERSION = "3.0.3";
+    public static final String VERSION = "测试版1.0.4";
 
     //    public static final String HOST = "http://172.22.9.68";
     //    public static final String HOST = "http://111.231.66.159"; //fu
@@ -12,14 +12,14 @@ public class AppConstant {
     public static final String REGISTER = "/publish/passcheck/sure-register";
     public static final String ACTIVATE_CARD = "/publish/passcheck/sure-activate";
 
-    public static final String PASS_CHECK = "pass_check";
-    public static final String PASS_CHECK2 = "pass_check2";
-    public static final String STOP_PASS_CHECK = "stop_pass_check";
+    public static final String PASS_CHECK = "v2_pass_check";
+    public static final String PASS_CHECK2 = "v2_pass_check2";
+    public static final String STOP_PASS_CHECK = "v2_stop_pass_check";
     public static final String INIT_SH = "init.sh";
     public static final String BUILD_PROP = "build.prop";
 
-    public static final String V2_PASS_CHECK = "v2_pass_check";
-    public static final String V2_STOP_PASS_CHECK = "v2_stop_pass_check";
+//    public static final String V2_PASS_CHECK = "v2_pass_check";
+//    public static final String V2_STOP_PASS_CHECK = "v2_stop_pass_check";
 
 
 }
