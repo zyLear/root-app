dc845e7b0dc28265de5a30803a770c75d4f86711
xiezongyu <xiezongyu@changingedu.com>
2018-12-29 12:07:55 +0800

    1.0.6

--- root-app/app/src/main/java/com/zylear/root/rootapp/constants/AppConstant.java
+++ root-app/app/src/main/java/com/zylear/root/rootapp/constants/AppConstant.java
@@ -2,7 +2,7 @@ package com.zylear.root.rootapp.constants;
 
 public class AppConstant {
 
-    public static final String VERSION = "测试版1.0.5";
+    public static final String VERSION = "测试版1.0.6";
 
         public static final String HOST = "http://172.22.9.68";
     //    public static final String HOST = "http://111.231.66.159"; //fu
